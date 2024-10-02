cd9f72605765073f05fb6f755427f371997a8207
Vitoria97419 <vitoria.antunes974@gmail.com>
2024-10-01 21:22:00 -0300

    Add files via upload

---
+++ Aula 2/exemplo6.py
@@ -0,0 +1,6 @@
+#Programa IMC
+h = float (input("Digite a sua altura :"))
+m = (72.7 * h) -58
+f = (62.1 * h) -44.7
+print ( F"O peso ideal para os homens é{m:.2f} e o peso ideal para as mulheres é
+mulheres é {f:.2f}")
